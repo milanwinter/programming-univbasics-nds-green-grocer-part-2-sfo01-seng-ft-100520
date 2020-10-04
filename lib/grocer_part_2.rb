8ce05c14a92e3f03853163d1998b2d556ed60ece
Milan Winter <milanwinter8@gmail.com>
2020-10-04 21:57:20 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,10 +1,29 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  cart.collect do |item|
-    if cart[:item] == coupons[:item]
+  count = 0
+  while count < coupons.length
+    cart_item = find_item_by_name_in_collection(coupons[count][:item],cart)
+    coupon_item = "#{coupons[counter][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(coupon_item,cart)
+    if cart_item && cart_item[:count] >= coupons[count][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon[:count] += coupons[count][:num]
+        cart_item[:count] -= coupons[count][:num]
+      else
+        cart_item_with_coupon = {
+          :item => coupon_item,
+          :price => coupons[count][:cost]/coupons[count][:num],
+          :clearance => cart_item[:clearance],
+          :count => coupons[counter][:num]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupon[count][:num]
+      end
     end
+    count += 1
   end
+  cart
 end
 
 def apply_clearance(cart)
